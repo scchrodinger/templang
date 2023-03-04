5bdefaaa8fe36956082cfaf22d159a2201483757
Schrodinger <118077794+schrodingerwastaken@users.noreply.github.com>
2023-03-04 11:14:09 +0300

    Update spanishlang.py

--- examples/spanishlang.py
+++ examples/spanishlang.py
@@ -4,46 +4,46 @@ path = sys.argv[1]
 #fill *** with your own file extention
 file = path;
 f = open(file, "r", encoding="utf-8")
-if file[len(file) - 4:] != "***":
-    print("transpiler can just open *** files");
+if file[len(file) - 4:] != ".spalang":
+    print("transpiler can just open .spalang files");
     quit()
 
 #fil the blanks with your own keywords
 main_lang = [
-        ("", "input"
-        ("", "elif"),
-        ("", "as"),
-        ("", "import"),
-        ("", "else"),
-        ("", "if"),
-        ("", "print"),
-        ("", "try"),
-        ("", "except"),
-        ("", "def"),
-        ("", "class"),
-        ("", "return"),
-        ("", "range"),
-        ("", "yield"),
-        ("", "from"),
-        ("", "continue"),
-        ("", "len"),
-        ("", "with"),
-        ("", "not"),
-        ("", "is"),
-        ("", "in"),
-        ("", "for"),
-        ("", "assert"),
-        ("", "pass"),
-        ("", "or"),
-        ("", "True"),
-        ("", "False"),
-        ("", "while"),
-        ("", "raise"),
-        ("", "and"),
-        ("", "None"),
-        ("", "exit"),
-        ("", "break"),
-        ("", "open"),
+        ("aporte", "input"
+        ("más si", "elif"),
+        ("como", "as"),
+        ("importar", "import"),
+        ("demás", "else"),
+        ("si", "if"),
+        ("imprimir", "print"),
+        ("intentar", "try"),
+        ("excepto", "except"),
+        ("definir", "def"),
+        ("clase", "class"),
+        ("devolver", "return"),
+        ("rango", "range"),
+        ("producir", "yield"),
+        ("de", "from"),
+        ("continuar", "continue"),
+        ("longitud", "len"),
+        ("con", "with"),
+        ("no", "not"),
+        ("es", "is"),
+        ("en", "in"),
+        ("para", "for"),
+        ("afirmar", "assert"),
+        ("aprobar", "pass"),
+        ("o", "or"),
+        ("Verdadera", "True"),
+        ("Falsa", "False"),
+        ("mientras", "while"),
+        ("aumentar", "raise"),
+        ("y", "and"),
+        ("Ninguna", "None"),
+        ("salida", "exit"),
+        ("romper", "break"),
+        ("abierta", "open"),
 ]
 
 content = f.read()
